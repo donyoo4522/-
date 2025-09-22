2254b9e9727b4780f573212a62359b88c1ccc120
Pan <donyoo4522@163.com>
2025-09-22 15:27:19 +0800

    code

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,22 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+
+        const val TOKEN = "He4uaJepLXU1o0WK" // 您的彩云天气API令牌
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
+}
